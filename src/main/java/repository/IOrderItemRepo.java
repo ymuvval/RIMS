2b0e401dbeeaebef68abfa464b0524f3ea43fd9a
Eruva <oeruva@ilstu.edu>
2022-12-01 13:37:04 -0600

    added new repo classes

---
+++ src/main/java/repository/IOrderItemRepo.java
@@ -0,0 +1,14 @@
+package repository;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import model.OrderItem;
+
+public interface IOrderItemRepo {
+	abstract public ArrayList<OrderItem> List(Integer orderId) throws SQLException;
+	abstract public void Add(OrderItem orderItem) throws SQLException;
+	abstract public void AddBatch(ArrayList<OrderItem> orderItems) throws SQLException;
+	abstract public void Update(OrderItem orderItem) throws SQLException;
+	abstract public void Delete(Integer id) throws SQLException;
+}
